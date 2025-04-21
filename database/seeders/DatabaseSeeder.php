32cfe216a1f9a76cc9ab312cc7f333f7d6a343b3
Mastayxz <gustibagus826@gmail.com>
2025-04-21 10:25:00 +0800

    modify task

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        // Category::factory()->count(5)->create();
-        // Post::factory()->count(10)->create();
+        // User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password'), // Ganti password sesuai kebutuhan
+                'role' => 'admin', // pastikan kolom 'role' sudah ada
+            ]
+        );
+        Category::factory()->count(5)->create();
+        Post::factory()->count(10)->create();
     }
 }
